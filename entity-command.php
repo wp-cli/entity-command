bfd32a66636f212b9829c259de4516f01076aad3
Daniel Bachhuber <daniel@handbuilt.co>
2017-04-04 16:39:22 -0700

    Restore `Site_Option_Command` and `User_Session_Command`
    These commands need to be included in this repo because of registry race
    conditions

--- entity-command.php
+++ entity-command.php
@@ -26,6 +26,13 @@ WP_CLI::add_command( 'post meta', 'Post_Meta_Command' );
 WP_CLI::add_command( 'post term', 'Post_Term_Command' );
 WP_CLI::add_command( 'post-type', 'Post_Type_Command' );
 WP_CLI::add_command( 'site', 'Site_Command' );
+WP_CLI::add_command( 'site option', 'Site_Option_Command', array(
+	'before_invoke' => function() {
+		if ( !is_multisite() ) {
+			WP_CLI::error( 'This is not a multisite install.' );
+		}
+	}
+) );
 WP_CLI::add_command( 'taxonomy', 'Taxonomy_Command' );
 WP_CLI::add_command( 'term', 'Term_Command' );
 WP_CLI::add_command( 'term meta', 'Term_Meta_Command', array(
@@ -37,4 +44,12 @@ WP_CLI::add_command( 'term meta', 'Term_Meta_Command', array(
 );
 WP_CLI::add_command( 'user', 'User_Command' );
 WP_CLI::add_command( 'user meta', 'User_Meta_Command' );
+WP_CLI::add_command( 'user session', 'User_Session_Command', array(
+	'before_invoke' => function() {
+		if ( \WP_CLI\Utils\wp_version_compare( '4.0', '<' ) ) {
+			WP_CLI::error( "Requires WordPress 4.0 or greater." );
+		}
+	})
+);
+
 WP_CLI::add_command( 'user term', 'User_Term_Command' );
