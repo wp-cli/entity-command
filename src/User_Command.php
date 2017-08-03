f1a0db06d1fe41121156e57e17010e19f8ab08cb
Govind Kumar <unavailable2010@gmail.com>
2017-08-04 01:09:58 +0530

    Added fields for the wp_create_user()

--- src/User_Command.php
+++ src/User_Command.php
@@ -302,11 +302,38 @@ class User_Command extends \WP_CLI\CommandWithDBObject {
 	 * [--display_name=<name>]
 	 * : The display name.
 	 *
+	 * [--user_nicename=<nice_name>]
+	 * : A string that contains a URL-friendly name for the user. The default is the user's username.
+	 *
+	 * [--user_url=<url>]
+	 * : A string containing the user's URL for the user's web site.
+	 *
+	 * [--user_email=<email>]
+	 * : A string containing the user's email address.
+	 *
+	 * [--nickname=<nickname>]
+	 * : The user's nickname, defaults to the user's username.
+	 *
 	 * [--first_name=<first_name>]
 	 * : The user's first name.
 	 *
 	 * [--last_name=<last_name>]
 	 * : The user's last name.
+	 *
+	 * [--description=<description>]
+	 * : A string containing content about the user.
+	 *
+	 * [--rich_editing=<rich_editing>]
+	 * : A string for whether to enable the rich editor or not. False if not empty.
+	 *
+	 * [--jabber=<jabber>]
+	 * : User's Jabber account.
+	 *
+	 * [--aim=<aim>]
+	 * : User's AOL IM account.
+	 *
+	 * [--yim=<yim>]
+	 * : User's Yahoo IM account.
 	 *
 	 * [--send-email]
 	 * : Send an email to the user with their new account details.
