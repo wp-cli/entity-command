f652c58d43635c2a83fe9c78e5dee37653fc5e96
Govind Kumar <unavailable2010@gmail.com>
2017-08-04 00:26:50 +0530

    Added field with description for wp_update_user()

--- src/User_Command.php
+++ src/User_Command.php
@@ -418,6 +418,54 @@ class User_Command extends \WP_CLI\CommandWithDBObject {
 	 * <user>...
 	 * : The user login, user email or user ID of the user(s) to update.
 	 *
+	 * --user_pass=<login>
+	 * : A string that contains the plain text password for the user.
+	 *
+	 * --user_login=<pass>
+	 * : A string that contains the user's username for logging in.
+	 *
+	 * --user_nicename=<nice_name>
+	 * : A string that contains a URL-friendly name for the user. The default is the user's username.
+	 *
+	 * --user_url=<url>
+	 * : A string containing the user's URL for the user's web site.
+	 *
+	 * --user_email=<email>
+	 * : A string containing the user's email address.
+	 *
+	 * --display_name=<display>
+	 * : A string that will be shown on the site. Defaults to user's username.
+	 *
+	 * --nickname=<nickname>
+	 * : The user's nickname, defaults to the user's username.
+	 *
+	 * --first_name=<first_name>
+	 * : The user's first name.
+	 *
+	 * --last_name=<last_name>
+	 * : The user's last name.
+	 *
+	 * --description=<description>
+	 * : A string containing content about the user.
+	 *
+	 * --rich_editing=<rich_editing>
+	 * : A string for whether to enable the rich editor or not. False if not empty.
+	 *
+	 * --user_registered=<register_date>
+	 * : The date the user registered. Format is Y-m-d H:i:s.
+	 *
+	 * --role=<role>
+	 * : A string used to set the user's role.
+	 *
+	 * --jabber=<jabber>
+	 * : User's Jabber account.
+	 *
+	 * --aim=<aim>
+	 * : User's AOL IM account.
+	 *
+	 * --yim=<yim>
+	 * : User's Yahoo IM account.
+	 *
 	 * --<field>=<value>
 	 * : One or more fields to update. For accepted fields, see wp_update_user().
 	 *
