68396519f2b53bc3cecb44152aef3a5eccbc10ac
Sidsector9 <siddharth.thevaril@gmail.com>
2017-08-04 13:50:23 +0530

    Added examples for option get command

--- src/Option_Command.php
+++ src/Option_Command.php
@@ -51,6 +51,18 @@ class Option_Command extends WP_CLI_Command {
 	 *     $ wp option get home
 	 *     http://example.com
 	 *
+	 *     # Get blog description.
+	 *     $ wp option get blogdescription
+	 *     A random blog description
+	 *
+	 *     # Get blog name
+	 *     $ wp option get blogname
+	 *     A random blog name
+	 *
+	 *     # Get admin email.
+	 *     $ wp option get admin_email
+	 *     someone@example.com
+	 *
 	 *     # Get option in JSON format.
 	 *     $ wp option get active_plugins --format=json
 	 *     {"0":"dynamically-dynamic-sidebar\/dynamically-dynamic-sidebar.php","1":"monster-widget\/monster-widget.php","2":"show-current-template\/show-current-template.php","3":"theme-check\/theme-check.php","5":"wordpress-importer\/wordpress-importer.php"}
