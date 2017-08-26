cd2774ef0135ae75689414b7dea8c64334694e67
bunty <bn.bhandari90@gmail.com>
2017-08-26 22:51:22 +0530

    Improve sorting function

--- src/WP_CLI/CommandWithMeta.php
+++ src/WP_CLI/CommandWithMeta.php
@@ -88,39 +88,43 @@ abstract class CommandWithMeta extends \WP_CLI_Command {
 		$order   = Utils\get_flag_value( $assoc_args, 'order' );
 		$orderby = Utils\get_flag_value( $assoc_args, 'orderby' );
 
-		// Sort array.
-		if ( 'meta_key' === $orderby ) { // Sort by meta_key.
-			if ( 'asc' === $order ) {
-				usort( $items, function( $a, $b ) {
-					if ( $a->meta_key == $b->meta_key ) {
-						return 0;
+		if ( 'id' !== $orderby ) {
+			usort( $items, function ( $a, $b ) use ( $orderby, $order ) {
+				// Sort array.
+				if ( 'meta_key' === $orderby ) { // Sort by meta_key.
+					if ( 'asc' === $order ) {
+
+						if ( $a->meta_key == $b->meta_key ) {
+							return 0;
+						}
+						return ( $a->meta_key < $b->meta_key ) ? -1 : 1;
+
+					} else {
+
+						if ( $a->meta_key == $b->meta_key ) {
+							return 0;
+						}
+						return ( $a->meta_key > $b->meta_key ) ? -1 : 1;
+
 					}
-					return ( $a->meta_key < $b->meta_key ) ? -1 : 1;
-				});
-			} else {
-				usort( $items, function( $a, $b ) {
-					if ( $a->meta_key == $b->meta_key ) {
-						return 0;
+				} elseif ( 'meta_value' === $orderby ) { // Sort by meta_value.
+					if ( 'asc' === $order ) {
+
+						if ( $a->meta_value == $b->meta_value ) {
+							return 0;
+						}
+						return ( $a->meta_value < $b->meta_value ) ? -1 : 1;
+
+					} else {
+
+						if ( $a->meta_value == $b->meta_value ) {
+							return 0;
+						}
+						return ( $a->meta_value > $b->meta_value ) ? -1 : 1;
+
 					}
-					return ( $a->meta_key > $b->meta_key ) ? -1 : 1;
-				});
-			}
-		} elseif ( 'meta_value' === $orderby ) { // Sort by meta_value.
-			if ( 'asc' === $order ) {
-				usort( $items, function( $a, $b ) {
-					if ( $a->meta_value == $b->meta_value ) {
-						return 0;
-					}
-					return ( $a->meta_value < $b->meta_value ) ? -1 : 1;
-				});
-			} else {
-				usort( $items, function( $a, $b ) {
-					if ( $a->meta_value == $b->meta_value ) {
-						return 0;
-					}
-					return ( $a->meta_value > $b->meta_value ) ? -1 : 1;
-				});
-			}
+				}
+			});
 		} elseif ( 'id' === $orderby && 'desc' === $order ) { // Sort by default descending.
 			krsort( $items );
 		}
