82c4e4ac30262a7eebcbd44f71968acd299299ba
Barrett Golding <mbox@hearingvoices.com>
2017-09-08 15:16:26 -0600

    Update Post_Type_Command.php

--- src/Post_Type_Command.php
+++ src/Post_Type_Command.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * Retrieves details on the site's registered post-types.
+ * Retrieves details on the site's registered post types.
  *
- * Get details on WordPress' built-in and [custom post types](https://developer.wordpress.org/plugins/post-types/).
+ * Get information on the WordPress' built-in and the site's [custom post types](https://developer.wordpress.org/plugins/post-types/).
  *
  * ## EXAMPLES
  *
