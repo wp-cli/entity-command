24dec4aa1ae36ca28d3f6fa7b948c0da0a5f0e1c
Daniele Scasciafratte <mte90net@gmail.com>
2018-07-20 17:01:41 +0200

    fix #175

--- src/Post_Command.php
+++ src/Post_Command.php
@@ -710,7 +710,11 @@ class Post_Command extends \WP_CLI\CommandWithDBObject {
 			$post_author = $user_fetcher->get_check( $post_author )->ID;
 		}
 
-		if ( \WP_CLI\Utils\get_flag_value( $assoc_args, 'post_content' ) ) {
+		if ( !empty( \WP_CLI\Utils\get_flag_value( $assoc_args, 'post_content' ) ) ) {
+			WP_CLI::error( "The parameter `post_content` reads from STDIN." );
+		}
+		
+		if ( \WP_CLI\Entity\Utils::has_stdin() && \WP_CLI\Utils\get_flag_value( $assoc_args, 'post_content' ) ) {
 			$post_content = file_get_contents( 'php://stdin' );
 		}
 
