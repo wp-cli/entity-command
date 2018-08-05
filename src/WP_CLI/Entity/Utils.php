1bed790ccb08dc88f225984e9a6ce556c153b79b
Alain Schlesser <alain.schlesser@gmail.com>
2018-08-05 19:31:45 +0200

    Revert "Fix bug in user import-csv STDIN detection"
    This reverts commit 2cbd22c157b47b11f8cf84623d6d57b35af0a75d.

--- src/WP_CLI/Entity/Utils.php
+++ src/WP_CLI/Entity/Utils.php
@@ -10,25 +10,13 @@ class Utils {
 	 * @return bool
 	 */
 	public static function has_stdin() {
-		$handle  = fopen( 'php://stdin', 'rb' );
+		$handle  = fopen( 'php://stdin', 'r' );
 		$read    = array( $handle );
 		$write   = null;
 		$except  = null;
 		$streams = stream_select( $read, $write, $except, 0 );
-		$fstat   = fstat( $handle );
-
 		fclose( $handle );
 
-		if ( $streams !== 1 ) {
-			// No stream on STDIN.
-			return false;
-		}
-
-		if ( isset( $fstat['size'] ) && $fstat['size'] === 0 ) {
-			// A stream on STDIN was detected but has a size of 0 bytes.
-			return false;
-		}
-
-		return true;
+		return 1 === $streams;
 	}
 }
