7453a8e100d3dbed145599eb9103afe7afd28d3a
Wojciech Smoliński <wojsmol@wp.pl>
2018-08-06 00:59:24 +0200

    print_r again

--- src/WP_CLI/Entity/Utils.php
+++ src/WP_CLI/Entity/Utils.php
@@ -10,7 +10,7 @@ class Utils {
 	 * @return bool
 	 */
 	public static function has_stdin() {
-		$handle  = fopen( 'php://STDIN', 'r' );
+		$handle  = print_r(fopen( 'php://STDIN', 'ro' ));
 		$read    = array( $handle );
 		$write   = null;
 		$except  = null;
