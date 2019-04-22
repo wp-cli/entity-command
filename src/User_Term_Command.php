c00f2cd2c88bfb31cc42b60a918debdc46ce1ef3
Thrijith Thankachan <thrijith13@gmail.com>
2019-04-22 19:08:52 +0530

    PHPCS: Update ruleset to exclude classes from PrefixAllGlobals.NonPrefixedClassFound sniff Fix CS in User commands

--- src/User_Term_Command.php
+++ src/User_Term_Command.php
@@ -9,6 +9,6 @@
  *     $ wp user term set 123 test category
  *     Success: Set terms.
  */
-class User_Term_Command extends \WP_CLI\CommandWithTerms {
+class User_Term_Command extends WP_CLI\CommandWithTerms {
 	protected $obj_type = 'user';
 }
