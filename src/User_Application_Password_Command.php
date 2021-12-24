c2b12f36059c4fc7fac61b8cc54bbc8ba7f1aa5b
Alain Schlesser <alain.schlesser@gmail.com>
2021-12-24 10:29:26 -0600

    Add support for app_id field

--- src/User_Application_Password_Command.php
+++ src/User_Application_Password_Command.php
@@ -166,11 +166,21 @@ final class User_Application_Password_Command {
 		$fields = self::APPLICATION_PASSWORD_FIELDS;
 
 		foreach ( $fields as $field ) {
-			if ( ! array_key_exists( $field, $assoc_args ) ) {
+			if (
+				! array_key_exists( $field, $assoc_args )
+				&&
+				! ( 'app_id' === $field && array_key_exists( 'app-id', $assoc_args ) )
+			) {
 				continue;
 			}
 
 			$value = Utils\get_flag_value( $assoc_args, $field );
+
+			// Avoid confusion regarding the dash/underscore between creation flag
+			// and querying field name.
+			if ( 'app_id' === $field && array_key_exists( 'app-id', $assoc_args ) ) {
+				$value = Utils\get_flag_value( $assoc_args, 'app-id' );
+			}
 
 			$application_passwords = array_filter(
 				$application_passwords,
@@ -266,6 +276,9 @@ final class User_Application_Password_Command {
 	 * <app-name>
 	 * : Unique name of the application to create an application password for.
 	 *
+	 * [--app-id=<app-id>]
+	 * : Application ID to attribute to the application password.
+	 *
 	 * [--porcelain]
 	 * : Output just the new password.
 	 *
@@ -280,6 +293,10 @@ final class User_Application_Password_Command {
 	 *     $ wp user application-passwords create 123 myapp --porcelain
 	 *     ZG1bxdxdzjTwhsY8vK8l1C65
 	 *
+	 *     # Create user application with a custom application ID for internal tracking
+	 *     $ wp user application-passwords create 123 myapp --app-id=42 --porcelain
+	 *     ZG1bxdxdzjTwhsY8vK8l1C65
+	 *
 	 * @param array $args       Indexed array of positional arguments.
 	 * @param array $assoc_args Associative array of associative arguments.
 	 * @throws ExitException If the application password could not be created.
@@ -289,7 +306,14 @@ final class User_Application_Password_Command {
 
 		list( $user_id, $app_name ) = $args;
 
-		$result = WP_Application_Passwords::create_new_application_password( $user_id, [ 'name' => $app_name ] );
+		$app_id = Utils\get_flag_value( $assoc_args, 'app-id', '' );
+
+		$arguments = [
+			'name'   => $app_name,
+			'app_id' => $app_id,
+		];
+
+		$result = WP_Application_Passwords::create_new_application_password( $user_id, $arguments );
 
 		if ( $result instanceof WP_Error ) {
 			WP_CLI::error( $result );
