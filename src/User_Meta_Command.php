aa8c90e86b2ab3212c4ae593bb16ad9bd958e4dc
Daniel Bachhuber <daniel@bachhuber.co>
2022-08-26 09:19:29 -0700

    Document `primary_blog` meta key for users

--- src/User_Meta_Command.php
+++ src/User_Meta_Command.php
@@ -131,6 +131,10 @@ class User_Meta_Command extends CommandWithMeta {
 	 *     # Get user meta
 	 *     $ wp user meta get 123 bio
 	 *     Mary is an WordPress developer.
+	 *
+	 *     # Get the primary site of a user (for multisite)
+	 *     $ wp user meta get 2 primary_blog
+	 *     3
 	 */
 	public function get( $args, $assoc_args ) {
 		$args = $this->replace_login_with_user_id( $args );
