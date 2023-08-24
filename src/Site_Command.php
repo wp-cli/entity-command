956f32046227f63e3b11ad020d454645b344c801
Tunbosun Ayinla <tayinla@gmail.com>
2023-08-24 14:16:23 -0400

    Add slug as an available parameter to 'wp site unspam' command

--- src/Site_Command.php
+++ src/Site_Command.php
@@ -722,8 +722,11 @@ class Site_Command extends CommandWithDBObject {
 	 *
 	 * ## OPTIONS
 	 *
-	 * <id>...
-	 * : One or more IDs of sites to remove from spam.
+	 * [<id>...]
+	 * : One or more IDs of sites to remove from spam. If not provided, you must set the --slug parameter.
+	 *
+	 * [--slug=<slug>]
+	 * : Path of the blog to be removed from spam. Subdomain on subdomain installs, directory on subdirectory installs.
 	 *
 	 * ## EXAMPLES
 	 *
@@ -732,8 +735,18 @@ class Site_Command extends CommandWithDBObject {
 	 *
 	 * @subcommand unspam
 	 */
-	public function unspam( $args ) {
-		$this->update_site_status( $args, 'spam', 0 );
+	public function unspam( $args, $assoc_args ) {
+		if ( isset( $assoc_args['slug'] ) ) {
+			$blog = get_blog_details( trim( $assoc_args['slug'], '/' ) );
+
+			if ( ! $blog ) {
+				WP_CLI::error( 'Site not found.' );
+			}
+
+			$this->update_site_status( [ $blog->blog_id ], 'spam', 0 );
+		} else {
+			$this->update_site_status( $args, 'spam', 0 );
+		}
 	}
 
 	/**
