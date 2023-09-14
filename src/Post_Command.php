83b9db9172e2954c84bbb319a23d260fd2dacbc8
Rafael Zaleski <rafaelzaleski@users.noreply.github.com>
2023-09-14 00:51:34 -0300

    Assign a value to the post date if it is empty.

--- src/Post_Command.php
+++ src/Post_Command.php
@@ -764,6 +764,15 @@ class Post_Command extends CommandWithDBObject {
 		$post_data['post_date']     = $this->maybe_convert_hyphenated_date_format( $post_data['post_date'] );
 		$post_data['post_date_gmt'] = $this->maybe_convert_hyphenated_date_format( $post_data['post_date_gmt'] );
 
+		// Handle edge cases for how WP handle dates on wp_insert_post for older versions. If one of the values is empty, it's assigned to the current time. We need to avoid sending an empty value.
+		if ( isset( $post_data['post_date'] ) && empty( $post_data['post_date_gmt'] ) ) {
+			$post_data['post_date_gmt'] = get_gmt_from_date( $post_data['post_date'] );
+		}
+		
+		if ( isset( $post_data['post_date_gmt'] ) && empty( $post_data['post_date'] ) ) {
+			$post_data['post_date'] = get_date_from_gmt( $post_data['post_date_gmt'] );
+		}
+
 		if ( ! post_type_exists( $post_data['post_type'] ) ) {
 			WP_CLI::error( "'{$post_data['post_type']}' is not a registered post type." );
 		}
