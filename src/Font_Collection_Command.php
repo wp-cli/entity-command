6e4ff59d39bd8633e6833f01261e556b35248f78
Pascal Birchler <pascalb@google.com>
2026-08-06 11:03:13 +0200

    Fix newly reported PHPStan errors

--- src/Font_Collection_Command.php
+++ src/Font_Collection_Command.php
@@ -394,6 +394,7 @@ class Font_Collection_Command extends WP_CLI_Command {
 
 		$fields    = [ 'slug', 'name' ];
 		$formatter = new Formatter( $assoc_args, $fields, 'category' );
+		/** @var array<int, array<string, mixed>> $categories */
 		$formatter->display_items( $categories );
 	}
 
